edc32cdfe9ba20ccfb2dc33763d645ac88009a4c
ltakeshi <tuxtakeshi+pr@gmail.com>
2017-09-09 00:02:00 +0900

    ルール選択画面のテンプレートエンジン対応

--- select.php
+++ select.php
@@ -1,48 +1,42 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>PRMapSelectorNG</title>
-  <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+<?php
+require_once "vendor/autoload.php";
 
-  <link rel="stylesheet" href="pr_map_selector.css">
-
-</head>
-<body>
-  <form action="result.php" method="post">
-    <input type="hidden" name="" />
-    <input type="submit" name="commit" value="ルールを選択する" />
-    <div class="grid">
-
-      <?php
-      require_once "vendor/autoload.php";
-
-      $Maps = Spyc::YAMLLoad('yaml/pr_maps.yaml');
-      $Rules = Spyc::YAMLLoad('yaml/pr_rules.yaml');
-
-      $data = $_POST['map'];
+$loader = new Twig_Loader_Filesystem("layout/");
+$twig = new Twig_Environment($loader, array("cache" => "cache/", "debug" => true));
+$twig->addExtension(new Twig_Extension_Debug());
 
 
-      foreach ( array_keys($data) as $key ){
-        print '<label for="map_'.$key.'">';
-        print '<div class="element-item nbox">';
-        print '<img src="img/'.$key.'.jpg" alt="'.$Maps[$key]['name'].'" width="240" height="135" />';
-        print ''.($Maps[$key]['name']).'';
-        print '<br />';
-        print '<select name="map[]">';
+$Maps = Spyc::YAMLLoad('yaml/pr_maps.yaml');
+$Rules = Spyc::YAMLLoad('yaml/pr_rules.yaml');
+$mapids_names_rules = array();
 
-        foreach( $Maps[$key]['rules'] as $num ){
-          print '<option value="'.$Maps[$key]['name'].' : '.$Rules[$num].'">'.$Rules[$num].'</option>';
-        }
 
-        print '</select>';
-        print '</div>';
-        print '</label>';
-      }
-      ?>
-    </div>
-  </form>
-  <script src="js/main.js"></script>
-</bod>
-</html>
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $postdata = $_POST;
+}
+$keys = array_keys($postdata['map']);
+$mapids_names_rules = array();
+
+
+foreach($keys as $key){
+    $i=0;
+    $mapids_names_rules[$key]['mapid'] = $key;
+    $mapids_names_rules[$key]['mapname'] = $Maps[$key]['name'];
+    foreach ($Maps[$key]['rules'] as $rule){
+    $mapids_names_rules[$key]['rules'][$i]  = $Rules[$rule];
+    $i++;
+    }
+        
+}
+
+$page = array(
+    "datas" => $mapids_names_rules,
+);
+
+
+
+$template = $twig->load("select.html.twig");
+$template->display($page);
+
+
+?>
