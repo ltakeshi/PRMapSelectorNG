e6f90653e6864fe29c9caf0823f531b333b4ee53
ltakeshi <tuxtakeshi+pr@gmail.com>
2017-09-10 21:46:48 +0900

    indexページにてマップを選択した時点で``ルールを選択する"を押せるようにjsを追加

--- select.php
+++ select.php
@@ -5,18 +5,16 @@ $loader = new Twig_Loader_Filesystem("layout/");
 $twig = new Twig_Environment($loader, array("cache" => "cache/", "debug" => true));
 $twig->addExtension(new Twig_Extension_Debug());
 
-
 $Maps = Spyc::YAMLLoad('yaml/pr_maps.yaml');
 $Rules = Spyc::YAMLLoad('yaml/pr_rules.yaml');
 $mapids_names_rules = array();
 
-
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $postdata = $_POST;
 }
+
 $keys = array_keys($postdata['map']);
 $mapids_names_rules = array();
-
 
 foreach($keys as $key){
     $i=0;
@@ -26,17 +24,12 @@ foreach($keys as $key){
     $mapids_names_rules[$key]['rules'][$i]  = $Rules[$rule];
     $i++;
     }
-        
 }
 
 $page = array(
     "datas" => $mapids_names_rules,
 );
 
-
-
 $template = $twig->load("select.html.twig");
 $template->display($page);
-
-
 ?>
